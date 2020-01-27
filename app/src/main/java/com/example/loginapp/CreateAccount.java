895f0f6fd01f240979e4c49aa4871b2915295043
nickwjmz <57211842+nickwjmz@users.noreply.github.com>
2020-01-27 01:25:17 -0500

    activity_create_account.xml almost completed

--- app/src/main/java/com/example/loginapp/CreateAccount.java
+++ app/src/main/java/com/example/loginapp/CreateAccount.java
@@ -1,8 +1,12 @@
 package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,16 +14,8 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-
-
-
 
 }
 

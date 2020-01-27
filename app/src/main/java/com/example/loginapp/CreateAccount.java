1f94e97f5c39116136831883acf12c953771b1f6
nickwjmz <57211842+nickwjmz@users.noreply.github.com>
2020-01-26 19:32:12 -0500

    activity_create_account.xml

---
+++ app/src/main/java/com/example/loginapp/CreateAccount.java
@@ -0,0 +1,26 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+
+
+
+}
+
+

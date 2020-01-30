89da43598b3112ce8c57ec6a0e43bafc1337daa4
nickwjmz <57211842+nickwjmz@users.noreply.github.com>
2020-01-29 22:37:58 -0500

    Creating UserInfo XML image added

--- app/src/main/java/com/example/loginapp/UserInfo.java
+++ app/src/main/java/com/example/loginapp/UserInfo.java
@@ -3,9 +3,21 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import static android.R.*;
 
 public class UserInfo extends AppCompatActivity {
+/*
+    Spinner spinner = (Spinner) findViewById(R.id.sp_user_country);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.spinner_choose_country, layout.simple_spinner_item);
 
+    adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);   // Specify the layout to use when the list of choices appears
+    spinner.setAdapter(adapter);    // Apply the adapter to the spinner
+
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
